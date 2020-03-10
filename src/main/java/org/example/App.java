97edbc6f7df8fe7545d378da5cc10a592ce6f5fb
Jackie Xu <jackiexu2007@hotmail.com>
2020-03-10 15:06:17 -0700

    "New feature" test, increasing version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        if (args.length > 0) {
+            System.out.println("More arguments!");
+        }
     }
 }
 
